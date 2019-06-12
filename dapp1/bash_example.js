1a9ea15feece701099145ade92613686baeba465
shivani <shivu91293@gmail.com>
2019-06-11 20:01:36 -0500

    demo

---
+++ dapp1/bash_example.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+console.log("----dotenv variables-----");
+console.log(process.env.PK_ACCOUNT_1);
+console.log(process.env.USERNAME);
+console.log(process.env.PASSWORD);
+console.log(process.env.ADDRESS);
+console.log(process.env.MNEMONIC);
+console.log("----bash_profile variables----")
+console.log(process.env.PK_GLOBAL);
+console.log(process.env.SOME_PATH);
+console.log(process.env.DB_URL);
+console.log(process.env.AWS_ACCESS_KEY_ID);
+console.log(process.env.AWS_SECRET_KEY);
+
